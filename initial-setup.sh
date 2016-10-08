ef9182c53e32fd9c1e90a65579512d8c0fa71612
Christopher R. Palmer <crpalmer@gmail.com>
2016-10-08 13:37:59 -0400

    Add an autorun wrapper

--- initial-setup.sh
+++ initial-setup.sh
@@ -41,3 +41,27 @@ echo 'export EDITOR=vi' >> /etc/bash.bashrc
 echo "Installing packages"
 
 apt-get install libusb-dev mpg321
+
+echo "Setting up autorun"
+
+cat > /etc/rc.local <<EOF
+#!/bin/bash
+/root/autorun-wrapper.sh > /root/autorun.log 2>&1 &
+exit 0
+EOF
+
+cat > /root/autorun-wrapper.sh <<EOF
+#!/bin/bash
+if [ -x /root/autorun.sh ]
+then
+   while true
+   do
+      /root/autorun.sh
+      sleep 1
+   done
+else
+   echo "Autorun is disabled"
+fi
+EOF
+
+chmod +x /root/autorun-wrapper.sh
